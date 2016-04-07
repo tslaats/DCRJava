66899be6c88db219674cee27bf09a52d3aceba9c
tslaats <t.slaats@gmail.com>
2016-04-08 00:41:20 +0200

    Adding templates

--- DCRGraphs/src/net/dcrgraphs/logchecker/DCRTemplateFactory.java
+++ DCRGraphs/src/net/dcrgraphs/logchecker/DCRTemplateFactory.java
@@ -1,12 +1,56 @@
 package net.dcrgraphs.logchecker;
+import java.util.HashSet;
+
+
 import net.dcrgraphs.core.*;
 
 public class DCRTemplateFactory {
 	
-	static DCRGraph init()
+	static DCRGraph init(HashSet<String> events, String eventA)
 	{
 		DCRGraph g = new DCRGraph();
+		for(String e : events)
+		{
+			g.AddEvent(e);
+			if (!e.equals(eventA))
+				g.AddCondition(eventA, e);
+		}
+		g.marking = g.DefaultInitialMarking();		
 		return g;
 	}
+	
+	
+	static DCRGraph existence(HashSet<String> events, String eventA)
+	{
+		DCRGraph g = new DCRGraph();
+		for(String e : events)
+			g.AddEvent(e);
+		g.marking = g.DefaultInitialMarking();
+		g.marking.pending.add(eventA);
+		return g;
+	}
+	
+	
+	static DCRGraph absence(HashSet<String> events, String eventA)
+	{
+		DCRGraph g = new DCRGraph();
+		for(String e : events)
+			g.AddEvent(e);
+		g.marking = g.DefaultInitialMarking();
+		g.marking.included.remove(eventA);
+		return g;
+	}	
+	
+	
+	static DCRGraph absence2(HashSet<String> events, String eventA)
+	{
+		DCRGraph g = new DCRGraph();
+		for(String e : events)
+			g.AddEvent(e);
+		g.marking = g.DefaultInitialMarking();
+		g.AddExclude(eventA, eventA);
+		return g;
+	}		
+	
 
 }
